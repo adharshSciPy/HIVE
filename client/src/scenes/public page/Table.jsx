11e848a6c95a1cdc9109e57fc6fbf418cfcf251e
Adharsh D <adharshd100@gmail.com>
2023-02-24 08:30:17 +0530

    delete api cheythu not worked

--- client/src/scenes/public page/Table.jsx
+++ client/src/scenes/public page/Table.jsx
@@ -1,93 +1,116 @@
-import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import axios from 'axios'
-import uuid from 'react-uuid'
-import { useNavigate } from 'react-router-dom';
-import { Button } from '@mui/material';
-import Box from '@mui/material/Box';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
-import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
-
-
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import axios from "axios";
+import uuid from "react-uuid";
+import { Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import HighlightOffRoundedIcon from "@mui/icons-material/HighlightOffRounded";
+import CheckCircleOutlineRoundedIcon from "@mui/icons-material/CheckCircleOutlineRounded";
+import Cookies from "js-cookie";
+import { toast } from "react-toastify";
 
 export default function DataTable({ handleSubmit }) {
+  let onClickDelete;
+  //   const token = Cookies.get("token");
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZjA3Y2ZmZTEwZWE1MWE4YTFhMjBlNyIsInJvbGUiOiJwdWJsaWMiLCJpYXQiOjE2NzcxODAxMDgsImV4cCI6MTY3NzE4MzcwOH0.5OODxH73r0ra_DLAcfnT-boLmp_7ftxyq9heZkx-GkE";
+  // defining coloumns
+  const columns = [
+    { field: "_id", headerName: "Id", width: 300, hideable: false, hide: true },
+    { field: "date", headerName: "Date", width: 130 },
+    { field: "title", headerName: "Title", width: 300 },
+    {
+      field: "action",
+      width: 190,
+      headerName: "Action",
+      sortable: false,
+      renderCell: (params) => {
+        onClickDelete = (e) => {
+          e.stopPropagation(); // don't select this row after clicking
 
-   const navigate = useNavigate()
-   // defining coloumns
-   const columns = [
-      { field: '_id', headerName: 'Id', width: 300, hideable: false, hide: true },
-      { field: 'date', headerName: 'Date', width: 130 },
-      { field: 'title', headerName: 'Title', width: 300 },
-      {
-         field: 'action',
-         width: 190,
-         headerName: 'Action',
-         sortable: false,
-         renderCell: (params) => {
-            const onClick = (e) => {
-               e.stopPropagation(); // don't select this row after clicking
+          const api: GridApi = params.api;
+          const thisRow: Record<string, GridCellValue> = {};
 
-               const api: GridApi = params.api;
-               const thisRow: Record<string, GridCellValue> = {};
+          api
+            .getAllColumns()
+            .filter((c) => c.field !== "_check_" && !!c)
+            .forEach(
+              (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+            );
+          // alert(JSON.stringify(thisRow))
+          console.log(thisRow._id);
 
-               api
-                  .getAllColumns()
-                  .filter((c) => c.field !== '_check_' && !!c)
-                  .forEach(
-                     (c) => (thisRow[c.field] = params.getValue(params.id, c.field)),
-                  );
-               alert(JSON.stringify(thisRow))
+          axios
+            .delete(`http://localhost:5000/public/deleteClass/${thisRow._id}`, {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            })
+            .then((res) => {
+              console.log(res.data.message);
+              toast.success(res.data.message, {
+                position: toast.POSITION.TOP_CENTER,
+              });
+            })
+            .catch((err) => {
+              console.error(err);
+              toast.error(err.message, {
+                position: toast.POSITION.TOP_CENTER,
+              });
+            });
+        };
 
-               // navigate(`${item._id}`)
-            };
-
-            return <Box
-               sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  '& > *': {
-                     m: 1,
-                  },
-               }}
-            >
-               <ButtonGroup size="small" aria-label="small button group">
-                  <Button color="error"><HighlightOffRoundedIcon /></Button>
-                  <Button color="success"><CheckCircleOutlineRoundedIcon /></Button>
-               </ButtonGroup>
-            </Box>
-         },
+        return (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              "& > *": {
+                m: 1,
+              },
+            }}
+          >
+            <ButtonGroup size="small" aria-label="small button group">
+              <Button color="error" onClick={onClickDelete}>
+                <HighlightOffRoundedIcon />
+              </Button>
+              <Button color="success">
+                <CheckCircleOutlineRoundedIcon />
+              </Button>
+            </ButtonGroup>
+          </Box>
+        );
       },
-   ];
+    },
+  ];
 
+  const [rows, setRows] = React.useState([]);
+  React.useEffect(() => {
+    axios
+      .get("http://localhost:5000/public/getScheduledClass")
+      .then((res) => {
+        setRows(res.data.ScheduledClass);
+        //   console.log(res.data.ScheduledClass);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [handleSubmit, onClickDelete]);
 
-
-   const [rows, setRows] = React.useState([])
-   React.useEffect(() => {
-
-      axios.get('http://localhost:5000/public/getScheduledClass')
-         .then((res) => {
-            setRows(res.data.ScheduledClass)
-            console.log(res.data.ScheduledClass)
-         })
-         .catch((err) => {
-            console.error(err)
-         })
-   }, [handleSubmit])
-
-   return (
-      <div style={{ height: 400, width: '100%' }}>
-         <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-            getRowId={(row: any) => uuid()}
-            disableSelectionOnClick
-            disableColumnMenu
-            disableColumnSelector
-         />
-      </div>
-   );
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        getRowId={(row: any) => uuid()}
+        disableSelectionOnClick
+        disableColumnMenu
+        disableColumnSelector
+      />
+    </div>
+  );
 }
--- server/controllers/PublicController.js
+++ server/controllers/PublicController.js
@@ -1,5 +1,5 @@
 const ScheduleSchema = require("../models/scheduleSchema");
-const fs = require('fs')
+const fs = require("fs");
 
 module.exports = {
   scheduleClass: async (req, res, next) => {
@@ -10,76 +10,81 @@ module.exports = {
         title,
         time,
         date,
-        pdfName: 'Demo Pdf',
-        status: true
-      })
+        pdfName: "Demo Pdf",
+        status: true,
+      });
       if (!scheduleData) {
-        res.status(400).json({ message: 'Posting Failded' })
+        res.status(400).json({ message: "Posting Failded" });
       }
-      console.log(scheduleData)
-      res.status(200).json({ message: 'Posted Succesfully' })
-    }
-    catch (err) {
-      console.log(err)
-      res.status(500).json({ message: 'Server Error' })
+      console.log(scheduleData);
+      res.status(200).json({ message: "Posted Succesfully" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Server Error" });
     }
   },
 
-
-
   // api to get the scheduled class details in student
   getScheduledClass: async (req, res) => {
-    const ScheduledClass = await ScheduleSchema.find({ status: true })
+    const ScheduledClass = await ScheduleSchema.find({ status: true });
     // console.log(ScheduledClass)
 
     try {
       if (!ScheduledClass) {
-        res.status(400).json({ message: 'No Classes Scheduled' })
+        res.status(400).json({ message: "No Classes Scheduled" });
       }
-      res.status(200).json({ message: "Classes Scheduled", ScheduledClass })
-    }
-
-    catch (err) {
-      res.status(500).json({ message: 'Server Error' })
+      res.status(200).json({ message: "Classes Scheduled", ScheduledClass });
+    } catch (err) {
+      res.status(500).json({ message: "Server Error" });
     }
   },
 
-
-
   getClassHistory: async (req, res) => {
-    const classHistory = await ScheduleSchema.find({ status: false })
+    const classHistory = await ScheduleSchema.find({ status: false });
 
     try {
       if (!classHistory) {
-        res.status(400).json({ message: 'Class History Not Found' })
+        res.status(400).json({ message: "Class History Not Found" });
       }
 
-      res.status(200).json({ message: "Class History Founded", classHistory })
-    }
-
-    catch (err) {
-      res.status(500).json({ message: 'Server Error' })
+      res.status(200).json({ message: "Class History Founded", classHistory });
+    } catch (err) {
+      res.status(500).json({ message: "Server Error" });
     }
   },
 
-
   
+  // delete api
+  deleteClass: async (req, res) => {
+    const { _id } = req.params.id;
+    const deleteApi = await ScheduleSchema.deleteOne({ _id });
+
+    try {
+      if (!deleteApi) {
+        res.status(400).json({ message: "Delete Operation failed" });
+      }
+      res.status(200).json({ message: "Succesfully Deleted" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Server Error" });
+    }
+  },
+
   // api to download pdf
   downloadPdf: async (req, res) => {
-    const { fileName } = req.body
+    const { fileName } = req.body;
     const pdf = `/server/uploads/${fileName}`;
     const file = `${__dirname}/${pdf}`;
 
-
     try {
       if (!file) {
-        res.status(400).json({ message: 'Pdf Note Found' })
+        res.status(400).json({ message: "Pdf Note Found" });
       }
-      res.status(200).json({ message: 'Pdf Founded' })
-      res.download(file) // Set disposition and send it.
+      res.status(200).json({ message: "Pdf Founded" });
+      res.download(file); // Set disposition and send it.
+    } catch (err) {
+      res.status(400).json({ message: "Server Error" });
     }
-    catch (err) {
-      res.status(400).json({ message: 'Server Error' })
-    }
-  }
+  },
+
 };
